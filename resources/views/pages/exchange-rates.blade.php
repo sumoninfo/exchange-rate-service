00e03379ae62eca09e4ceffd20ebe5393d041a4e
Ekramul Islam Sumon <38817030+sumoninfo@users.noreply.github.com>
2024-10-01 15:10:24 +0600

    Update exchange-rates.blade.php

--- resources/views/pages/exchange-rates.blade.php
+++ resources/views/pages/exchange-rates.blade.php
@@ -21,7 +21,7 @@
         <div class="modal-dialog">
             <div class="modal-content">
                 <div class="modal-header">
-                    <h5 class="modal-title" id="detailsModalLabel">Currency Details</h5>
+                    <h5 class="modal-title" id="detailsModalLabel">Currency History</h5>
                     <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
                 </div>
                 <div class="modal-body">
